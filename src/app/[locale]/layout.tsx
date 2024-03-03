bb04c6bd8e7a9be46ded5b51ae8531755a316582
Sebastian Morales <sebastian-morales@sebasmoralesd.com>
2024-03-03 15:38:21 -0500

    [SCRUM-2] v.0.0.1 - Estructura inicial del proyecto

---
+++ src/app/[locale]/layout.tsx
@@ -0,0 +1,41 @@
+import "@/app/css/globals.css";
+import { ReactQueryProvider } from "@/app/providers/reactQuery";
+import { SpeedInsights } from "@vercel/speed-insights/next";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "",
+  description: "",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <ToastContainer
+          position="bottom-center"
+          autoClose={5000}
+          limit={3}
+          hideProgressBar={false}
+          newestOnTop
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+        />
+        <ReactQueryProvider>{children}</ReactQueryProvider>
+        <SpeedInsights />
+      </body>
+    </html>
+  );
+}
